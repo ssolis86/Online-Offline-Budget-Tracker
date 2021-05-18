107063d3408ae9164e83931973f3987d9d20a265
Stephen Solis <ssolis8619@gmail.com>
2021-05-17 22:52:53 -0500

    indexedDB & console.log objectStoreNames

--- public/index.js
+++ public/index.js
@@ -1,5 +1,29 @@
 let transactions = [];
 let myChart;
+let db;
+let budgetVersion;
+
+// Create a new db request for a "budget" database.
+const request = window.indexedDB.open("BudgetDB", budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB');
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
+};
+
+request.onsuccess = event => {
+  console.log(request.result);
+};
 
 fetch("/api/transaction")
   .then(response => {
@@ -144,6 +168,10 @@ function sendTransaction(isAdding) {
   });
 }
 
+const saveRecord = (record) => {
+  console.log('Save record invoked');
+  
+}
 document.querySelector("#add-btn").onclick = function() {
   sendTransaction(true);
 };
