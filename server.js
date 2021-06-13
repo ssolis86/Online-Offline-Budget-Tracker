cfdf00b2ed907c3eabec1d391a00b269c12028a0
ssolis1337 <71684140+ssolis86@users.noreply.github.com>
2021-06-12 22:33:42 -0500

    deploying

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -15,9 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.envMONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", 
+{
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
